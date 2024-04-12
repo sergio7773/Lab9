cb9f316a886b9b12c1b699db30035cba3fcc9234
jingtongzeng <jingtongzeng123@gmail.com>
2024-04-11 22:12:39 -0400

    made changes to decoder and everything works

--- lab9.py
+++ lab9.py
@@ -6,11 +6,21 @@ def encoder(password):
         encoded_password += str(digit)
     return encoded_password
 
-def decoder(password):
-    decoded_password = ""
-    for digit in password:
-        digit = int(digit)
-        digit -=3
+def decoder(encoded_password):
+    emptystring = ""
+    for i in encoded_password:
+        i = int(i)
+        if i == 2:
+            emptystring = emptystring + str(9)
+        elif i == 1:
+            emptystring = emptystring + str(8)
+        elif i == 0:
+            emptystring = emptystring + str(7)
+        else:
+            i -= 3
+            emptystring = emptystring + str(i)
+    return emptystring
+
 
 def main():
     encoder_on = True
@@ -27,8 +37,10 @@ def main():
             encoded_password = encoder(password)
             print("Your password has been encoded and stored!\n")
         elif option == 2:
+            decoded_password = decoder(encoded_password)
+            print(decoded_password)
             print(f"The encoded password is {encoded_password},"
-                  f" and the original password is {password}.\n")
+                  f" and the original password is {decoded_password}.\n")
         elif option == 3:
             encoder_on = False
 
